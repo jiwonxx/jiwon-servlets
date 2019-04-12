9493f6513c7108e917a18e766b421bbb054d6ca1
김지원 <jiwonx_x_@naver.com>
2019-04-12 18:21:39 +0900

    update

---
+++ src/main/java/com/jiwon/servlets/examples/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.jiwon.servlets.examples;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse; 
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = "김지원";
+		
+		request.setAttribute("name", name);
+		
+		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,
+				response);
+	}
+}
